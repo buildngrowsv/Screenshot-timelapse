d410ecfaab5fa5d77bb71c3f143293de498d1401
buildngrowsv <buildngrowsv@gmail.com>
2024-11-10 19:30:28 -0600

    First created xcode

---
+++ TimelapseApp/TimelapseApp/TimelapseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelapseAppApp.swift
+//  TimelapseApp
+//
+//  Created by A K on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TimelapseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
